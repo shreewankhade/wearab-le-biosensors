6bf498b0d580a966a6f63d1439207137e24673dc
shrikant wankhade <110273226+shreewankhade@users.noreply.github.com>
2024-02-29 00:13:15 +0530

    first commit

---
+++ Wearable_Biosensors_for_Human_Motion_Rehabilitation_and_Analysi.ino
@@ -0,0 +1,142 @@
+
+
+
+
+
+
+
+
+Servo servo; // create servo object to control a servo
+// variables will change:
+
+
+
+float duration_us, distance_cm;
+
+
+
+
+
+
+
+void setup() {
+
+
+
+  Serial.begin (9600);       // initialize serial port
+
+
+
+  pinMode(TRIG_PIN, OUTPUT); // set arduino pin to output mode
+
+
+
+ pinMode(ECHO_PIN, INPUT);  // set arduino pin to input mode
+
+
+
+  servo.attach(SERVO_PIN);   // attaches the servo on pin 9 to the servo object
+
+
+
+  servo.write(0);
+
+
+
+}
+
+
+
+
+
+
+
+void loop() {
+
+
+
+  // generate 10-microsecond pulse to TRIG pin
+
+
+
+  digitalWrite(TRIG_PIN, HIGH);
+
+
+
+  delayMicroseconds(200);
+
+
+
+  digitalWrite(TRIG_PIN, LOW);
+
+
+
+
+
+
+
+  // measure duration of pulse from ECHO pin
+
+
+
+  duration_us = pulseIn(ECHO_PIN, HIGH);
+
+
+
+  // calculate the distance
+
+
+
+  distance_cm = 0.017 * duration_us;
+
+
+
+
+
+
+
+  if(distance_cm < DISTANCE_THRESHOLD)
+
+
+
+    servo.write(90); // rotate servo motor to 90 degree
+
+
+
+  else
+
+
+
+    servo.write(0);  // rotate servo motor to 0 degree
+
+
+
+
+
+
+
+  // print the value to Serial Monitor
+
+
+
+  Serial.print("distance: ");
+
+
+
+  Serial.print(distance_cm);
+
+
+
+  Serial.println(" cm");
+
+
+
+
+
+
+
+  delay(3000);
+
+
+
+}
